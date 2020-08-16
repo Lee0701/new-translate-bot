2cf8619448ab2ded2bdb0795e790854673daecf2
Lee0701 <hgl1002@naver.com>
2020-08-17 07:33:07 +0900

    Update papago translator

--- translators/papago-translator.js
+++ translators/papago-translator.js
@@ -1,31 +1,61 @@
 
 const request = require('request')
 
+const crypto = require('crypto')
+const {v4: uuidv4} = require('uuid')
+
+const BASE_URL = 'https://papago.naver.com/apis/n2mt/translate'
+
+let uuid = null
+let key = ''
+
+const updateKey = () => {
+  key = 'v1.5.1_4dfe1d83c2'
+  uuid = uuidv4()
+}
+
 module.exports = function(text, fromLang, language, callback) {
   const from = fromLang === 'zh_CN' ? 'zh-CN' : fromLang === 'zh_TW' || fromLang === 'zh_HK' ? 'zh-TW' : fromLang.split('_')[0]
   const to = language === 'zh_CN' ? 'zh-CN' : language === 'zh_TW' || language === 'zh_HK' ? 'zh-TW' : language.split('_')[0]
 
-  const base = 'rlWxnJA0Vwc0paIyLCJkaWN0RGlzcGxheSI6NSwic291cmNlIjoi'
-  const str = '' + from + '","target":"' + to + '","text":"' + text + '"}'
-  const data = 'data=' + base + Buffer.from(str).toString('base64')
-
-  const options = {
-    url: 'https://papago.naver.com/apis/n2mt/translate',
-    method: 'POST',
-    headers: {
-      'User-Agent': 'Mozilla/5.0',
-      'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    },
-    body: data,
+  const generateOptions = () => {
+    const timestamp = new Date().getTime()
+    const hmac = crypto.createHmac('md5', key)
+    hmac.update(`${uuid}\n${BASE_URL}\n${timestamp}`)
+    const token = hmac.digest('base64')
+    const authorization = `PPG ${uuid}:${token}`
+    const data = `source=${from}&target=${to}&text=` + encodeURIComponent(text)
+  
+    return {
+      url: BASE_URL,
+      method: 'POST',
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Authorization': authorization,
+        'Timestamp': timestamp,
+      },
+      body: data,
+    }
   }
 
-  request(options, (err, res, body) => {
+  request(generateOptions(), (err, res, body) => {
     if(err) {
       console.error(err)
       callback(undefined)
-      return
     }
-    callback(JSON.parse(body).translatedText)
+    else if(res.statusCode === 403) {
+      updateKey()
+      request(generateOptions(), (err, res, body) => {
+        if(err) {
+          console.error(err)
+          callback(undefined)
+        }
+        else if(res.statusCode !== 200) callback(res.statusCode)
+        else callback(JSON.parse(body).translatedText)
+      })
+    }
+    else callback(JSON.parse(body).translatedText)
   })
 
 }
